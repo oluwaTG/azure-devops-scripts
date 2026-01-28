13779fc73f5df8a7de240cea1e6639f174638298
Ty <toyin@Mac.lan>
2026-01-28 15:51:38 +0000

    add argo cd ingress file

--- devops-helper/src/DevOpsHelper.Api/obj/Debug/net8.0/DevOpsHelper.Api.AssemblyInfo.cs
+++ devops-helper/src/DevOpsHelper.Api/obj/Debug/net8.0/DevOpsHelper.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevOpsHelper.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bea466376689923257f3cc5ed461519582a8a106")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45b1628cc5822bd4fc1a048ce8d5bd474d9f123a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevOpsHelper.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevOpsHelper.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
